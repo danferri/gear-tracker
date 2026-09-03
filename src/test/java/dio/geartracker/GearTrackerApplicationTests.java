19a74e92c9a27265f99ba5e2817e8a10094aadea
Daniel Ferri <the_rover@terra.com.br>
2026-09-03 13:41:37 -0300

    chore: initial project setup with Spring Boot and Gradle

---
+++ src/test/java/dio/geartracker/GearTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.geartracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
